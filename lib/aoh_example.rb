57c1a2ddf3837b32f6119d399e000178b7fe56ef
Rubens Fernandes <rubens.fernandes97@gmail.com>
2020-04-23 13:08:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,11 +25,11 @@ def literal_aoh
   # held in the constants
   
   workers = [
-    {name:  "Don Gately", occupation: "Live-in Staff" },
-    {name:  "Joelle van Dyne", occupation: "Radio Personality" }
-    {name:  "Pat Monteseian", occupation: "Staff" }
-    {name:  "Kate Gompert", occupation: "None" }
-    {name:  "Bruce Green", occupation: "Fan of Mildred" }
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+    { name:  "Pat Monteseian", occupation: "Staff" }
+    { name:  "Kate Gompert", occupation: "None" }
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
   
 workers
